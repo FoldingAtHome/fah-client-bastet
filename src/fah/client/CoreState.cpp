f02c5935f11f94199ad3ae4bd6c7c5fd4080df27
Joseph Coffland <joseph@cauldrondevelopment.com>
2019-11-07 21:48:41 -0800

    Updated code license tag

--- src/fah/client/CoreState.cpp
+++ src/fah/client/CoreState.cpp
@@ -2,13 +2,13 @@
 
                   This file is part of the Folding@home Client.
 
-           The fahclient runs Folding@home protein folding simulations.
-                    Copyright (c) 2001-2019, foldingathome.org
+          The fah-client runs Folding@home protein folding simulations.
+                    Copyright (c) 2001-2020, foldingathome.org
                                All rights reserved.
 
        This program is free software; you can redistribute it and/or modify
        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation; either version 2 of the License, or
+        the Free Software Foundation; either version 3 of the License, or
                        (at your option) any later version.
 
          This program is distributed in the hope that it will be useful,
