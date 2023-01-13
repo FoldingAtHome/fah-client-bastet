bb450b693fbcc5d1bfd147c768667637d5aebf7e
Joseph Coffland <joseph@cauldrondevelopment.com>
2023-01-13 16:15:38 +0200

    Updated copyrights

--- src/fah/client/glibc.c
+++ src/fah/client/glibc.c
@@ -1,3 +1,31 @@
+/******************************************************************************\
+
+                  This file is part of the Folding@home Client.
+
+          The fah-client runs Folding@home protein folding simulations.
+                    Copyright (c) 2001-2023, foldingathome.org
+                               All rights reserved.
+
+       This program is free software; you can redistribute it and/or modify
+       it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 3 of the License, or
+                       (at your option) any later version.
+
+         This program is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                   GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License along
+     with this program; if not, write to the Free Software Foundation, Inc.,
+           51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+                  For information regarding this software email:
+                                 Joseph Coffland
+                          joseph@cauldrondevelopment.com
+
+\******************************************************************************/
+
 // Replace GLIBC functions or force linking to older versions
 #if defined(__GNUC__) && !defined(__clang__) && !defined(_WIN32)
 #define _GNU_SOURCE
